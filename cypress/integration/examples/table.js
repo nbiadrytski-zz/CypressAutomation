3cc22471cdcf042351bf6c3167ba7bbf6ef1d3a0
Mikalai Biadrytski <nickbiadrytski@gmail.com>
2019-12-20 16:41:33 +0300

    Adds table search

--- cypress/integration/examples/table.js
+++ cypress/integration/examples/table.js
@@ -5,16 +5,23 @@
 // run one test from console (not headless electron): npx cypress run --headed --spec "cypress/integration/examples/Test1.js"
 // run one test from console in chrome: npx cypress run --browser chrome --spec "cypress/integration/examples/Test1.js"
 
+// https://www.udemy.com/course/cypress-tutorial/learn/lecture/15647522#content
+
 describe('My Second Test Suite', function() {
 
     it('My seconds test', function() {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
 
         // 'tr td:nth-child(2)'  --> tr is parent, td is child (select 2nd td out of 3)
-        cy.get('tr td:nth-child(2)').each(($elem, _index, _$list) => {
-            var courseText = $elem.text()
+        cy.get('tr td:nth-child(2)').each(($elem, index, _$list) => {
+            var courseText = $elem.text() // Jquery
             if(courseText.includes('Python')) {
-                
+                // eq() extracts the index of Python course
+                cy.get('tr td:nth-child(2)').eq(index).next().then(function(price) {
+                    // text() is not Cypress func, so Jquery promise needs to be resolved by then()
+                    var priceText = price.text()
+                    expect(priceText).to.equal('25')
+                })
             }
           })
     })
